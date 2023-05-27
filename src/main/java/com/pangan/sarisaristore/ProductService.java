abfb95dbf46e2eed6b7b099d86cdd7b0f8b76a12
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-05-27 17:33:14 +0800

    Added getting of all products

--- src/main/java/com/pangan/sarisaristore/ProductService.java
+++ src/main/java/com/pangan/sarisaristore/ProductService.java
@@ -1,4 +1,7 @@
 package com.pangan.sarisaristore;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getAllProducts();
 }
--- src/main/java/com/pangan/sarisaristore/ProductServiceMySql.java
+++ src/main/java/com/pangan/sarisaristore/ProductServiceMySql.java
@@ -1,7 +1,16 @@
 package com.pangan.sarisaristore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductServiceMySql implements ProductService{
+public class ProductServiceMySql implements ProductService {
+    @Autowired
+    private ProductRepository _productRepository;
+
+    public List<Product> getAllProducts() {
+        return _productRepository.findAll();
+    }
 }
