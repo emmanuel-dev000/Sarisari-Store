5f0e690fe349e538d5bedcbdd3c6219a0cf82863
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-05-28 09:56:33 +0800

    Added a sample script for list

--- src/main/resources/react/sarisaristore/src/App.js
+++ src/main/resources/react/sarisaristore/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import DefaultButton from './component/Button';
+import DefaultButton from "./component/Button";
+import ListWithAddItem from "./component/List";
 
 function App() {
   return (
     <div className="App">
         <DefaultButton />
+        <ListWithAddItem />
     </div>
   );
 }
--- src/main/resources/react/sarisaristore/src/component/Button.js
+++ src/main/resources/react/sarisaristore/src/component/Button.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function DefaultButton() {
-    return (
-        <Button 
-          variant="contained" 
-          onClick={() => { alert('clicked'); }}>
-            Get All Products
-        </Button>
-    );
+  const onGetAllProductsButtonClick = (e) => {
+    
+  };
+  return (
+      <Button 
+        variant="contained" 
+        onClick={() => { alert('clicked'); }}>
+          Get All Products
+      </Button>
+  );
 }
